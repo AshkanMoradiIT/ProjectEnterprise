9b7d7476ee26c31f766edc8489c2c59b25ab060e
Ashkan Moradi <info.ashkanmoradi@gmail.com>
2024-10-07 12:11:18 +0330

    Fix Bog in NHibernate

--- ProjectEnterprise/Models/NHibernate/Mapping/NHibernateHelper.cs
+++ ProjectEnterprise/Models/NHibernate/Mapping/NHibernateHelper.cs
@@ -15,13 +15,21 @@ namespace ProjectEnterprise.Models.NHibernate.Mapping
         public static void Initialize()
         {
             sessionFactory = new Configuration()
-                .Configure() 
+                .Configure()
                 .AddAssembly("ProjectEnterprise")
+                //.AddAssembly(typeof(NHibernateHelper).Assembly)
                 .BuildSessionFactory();
         }
 
         public static ISession OpenSession()
         {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\Nhibernate\Mapping\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var AllConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\Nhibernate\Mapping\Mapping.hbm.xml");
+            configuration.AddFile(AllConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+
             return sessionFactory.OpenSession();
         }
     }
